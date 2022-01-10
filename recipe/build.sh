0065f07a051dd42f51d61744e7b2a011d9f72da8
Brian Fulton-Howard <fultonh1@gmail.com>
2022-01-10 10:06:03 -0500

    Update for v6 (#12)
    * Update for v6. MNT: Re-rendered with conda-build 3.21.7, conda-smithy 3.16.1, and conda-forge-pinning 2022.01.09.15.48.10
    
    * more specific go compiler. MNT: Re-rendered with conda-build 3.21.7, conda-smithy 3.16.1, and conda-forge-pinning 2022.01.10.13.01.59

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-if [[ -f configure ]]; then
-  ./configure --prefix=$PREFIX
-  make
-  make check
-  make install
-else
-  make
-  make check
-  cp go/mlr $PREFIX/bin/
-  cp man/mlr.1 $PREFIX/man/man1/
-fi
+./configure --prefix=$PREFIX
+make
+make check
+make install
